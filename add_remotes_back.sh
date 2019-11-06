963e9af01bb82a2ead5bd05aab6d5d5d986682b6
Richard Skumat <gitlab@richardskumat.com>
2019-11-06 08:56:26 +0000

    update remotes [skip ci]

--- add_remotes_back.sh
+++ add_remotes_back.sh
@@ -1,3 +1,3 @@
 git remote add github git@github:richardskumat/ansible-role-nfs
 git remote add gitlab git@gitlab:richardskumat/ansible-role-nfs
-git remote add origin git@wsgitlab:qwe/ansible-role-nfs
+git remote add origin git@gitea:qwe/ansible-role-nfs
